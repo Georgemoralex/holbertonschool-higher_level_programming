8a3c698462a9fa3952e01c1bc31f63534720a66b
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-03-21 00:37:34 -0600

    pycode style

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def main():
-	print('"Programming is like building a multilingual puzzle')
+
+
+    print('"Programming is like building a multilingual puzzle')
 
 if __name__ == "__main__":
     main()
+    
