da1df7452f0c0a58b1dc7871132ebc54fcae78d0
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-04-07 22:21:14 -0600

    redo

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
-from add_0 import add
-
-def main():
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
-
-if __name__ == "__main__":
-    main()
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
