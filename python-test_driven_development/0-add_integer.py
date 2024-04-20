6bd9e83732ea6939307573649fbf4105fccc77f9
GeorgeMoralex <146600665+Georgemoralex@users.noreply.github.com>
2024-04-19 22:19:29 -0600

    function documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,13 +2,11 @@
 
 """
 Documentation
-
 Function that adds 2 integers. 
-Returns an integer: the addition of a and b
-
 """
 
 def add_integer(a, b=98):
+    """Returns an integer: the addition of a and b"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
